db203b988db153082d1c3418adcce54aa33ade12
yibaiershikexin <xinxin521125@hotmail.com>
2018-05-28 08:06:45 +0800

    python asyncio
    study sorce

--- python_asyncio/easy_sample.py
+++ python_asyncio/easy_sample.py
@@ -160,6 +160,123 @@ def test_task_1():
     [1, 2, 3, 4]
     successful!
     '''
-test_task_1()
+
+
+def test_async_callback():
+    '''callback's arg have one parameter: furture, it saved the task run detail info.
+    whether func_4 exception happen or success run, the call always run.
+    callback's furture.result() can accept exception, when receive a exception will raise to task
+    '''
+
+    @asyncio.coroutine
+    def func_4(*args, **kwargs):
+        print('func_3 args:%s kwargs:%s' % (str(args), kwargs))
+        # raise ValueError('xxxxxxx')
+        return {'args': args, 'kwargs': kwargs}
+
+    def func_cblk_1(furture, clbk_name, ):
+        '''callback func is meaningless to return any thing.
+          because none func can receive the callback return result'''
+        print('func_calk args:', clbk_name, furture)
+        print('furture dir:', dir(furture))
+        # if main programe raise exception, furture.result() will raise the same exception
+        print('run_result:', furture.result())
+        # callback return any thine is meanless
+        # return 'xxxxxxxxxxxxxx'
+
+    def func_cblk_2(arg1, furture):
+        '''
+        callback with args style of writing.
+            arg must write to frist ex: def callbck(arg1, arg2,...,, furture)
+        callback with kwargs style of writing:
+            kwargs must write after furture ex: def callback(furture, **kwargs)
+
+        'furture' is the default task's information, it call save the every task run detail.
+
+        '''
+        print('func_cblk args:', arg1, furture)
+        print('run_result:', furture.result())
+
+    task = asyncio.async(func_4(1, 2, 3, name='callback'))
+    # task can register multi callback func
+    task.add_done_callback(partial(func_cblk_1, clbk_name='****haha,good****'))
+    task.add_done_callback(partial(func_cblk_2, '****arg1,good****'))
+    loop.run_until_complete(task)
+
+    print('task result: %s' % task.result())
+
+
+def test_multi_tasks():
+    '''we can add a callback for a group multi task job'''
+
+    @asyncio.coroutine
+    def task_1(*args, **kwargs):
+        print('task_1 args:%s kwargs:%s' % (str(args), kwargs))
+        if not (args and kwargs):
+            sleep(1)
+        return {'args': args, 'kwargs': kwargs}
+
+    def cblk(furture, success_flg):
+        print('cblk func', success_flg)
+        print(furture.result())
+
+    multi_task = asyncio.gather(
+        task_1(1, 2, 3),
+        task_1(name='gather'),
+        task_1(1, 2, 4, run_sum='get total'),
+        task_1(),
+    )
+    multi_task.add_done_callback(partial(cblk, success_flg=True))
+    results = loop.run_until_complete(multi_task)
+    print('--------')
+    print(results)
+    for _result in results:
+        print(_result)
+        if _result.get('kwargs') == {}:
+            print('failed')
+            break
+    print('--------')
+
+
+def test_wait_task():
+    @asyncio.coroutine
+    def task_1(*args, **kwargs):
+        print('task_1 args:%s kwargs:%s' % (str(args), kwargs))
+        wait_time = kwargs.get('wait', 0)
+        print('sleep: %s' % wait_time)
+        # asyncio.sleep(wait_time)
+        sleep(wait_time)
+        return {'args': args, 'kwargs': kwargs}
+
+    @asyncio.coroutine
+    def task_2(*args, **kwargs):
+        print('task_1 args:%s kwargs:%s' % (str(args), kwargs))
+        wait_time = kwargs.get('wait', 0)
+        print('sleep: %s' % wait_time)
+        # asyncio.sleep(wait_time)
+        sleep(wait_time)
+        return {'args': args, 'kwargs': kwargs}
+
+    def cblk(furture):
+        print('cblk')
+        print(furture.result())
+
+    multi_task = asyncio.wait(
+        [
+            task_1(1, wait=1, name='1'),
+            task_1(2, wait=2, name='2'),
+            task_2(3, wait=3, name='3'),
+            task_2(4, wait=4, name='4'),
+        ],
+        timeout=1,
+        return_when=asyncio.FIRST_COMPLETED,
+        # return_when=asyncio.FIRST_EXCEPTION,
+    )
+    # multi_task.add_done_callback(cblk)
+    result = loop.run_until_complete(multi_task)
+    print(result)
+
+
+test_wait_task()
 
 print('successful!')
